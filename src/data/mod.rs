72f2ab37597be49300465808756c9bf313d95e42
Akshat Deshpande <akshatd18@outlook.com>
2022-12-16 04:35:46 +0000

    basic tui implementation

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,66 +1,61 @@
-use std::{future::Future};
+use std::future::Future;
 
-use reqwest::{Response, Error};
+use reqwest::{Error, Response};
 
-
-use crate::data::handlers::*;
 use self::structure::Resource;
-use serde_json::{Value};
+use crate::data::handlers::*;
+use serde_json::Value;
 
-pub mod structure;
 pub mod handlers;
+pub mod structure;
 
-
-pub async fn handleRequest(request: impl Future<Output = Result<Response, Error>>) -> String {
+pub async fn handle_request(request: impl Future<Output = Result<Response, Error>>) -> String {
     let result = match request.await {
-        Ok(result) => {result}
+        Ok(result) => result,
         Err(result) => {
             println!("API REQUEST: {}", result);
             std::process::exit(0);
         }
     };
     let body = match result.text().await {
-        Ok(body) => {body}
+        Ok(body) => body,
         Err(body) => {
             println!("FETCH BODY: {}", body);
             std::process::exit(0);
         }
     };
 
-
-    return body;
-
-
+    body
 }
 
 struct ProviderData {
     provider: Box<dyn Provider>,
-    list: Vec<Value>
+    list: Vec<Value>,
 }
 
 pub async fn search(filter: &str) -> Vec<Vec<Resource>> {
-   let providers: Vec<Box<dyn Provider>> = vec![
+    let providers: Vec<Box<dyn Provider>> = vec![
         Box::new(bukkit::BukkitHandler),
-        Box::new(spigot::SpigotHandler)
-   ];
-   let size: usize = 10; 
-   let mut data_size: usize = 0;
-   let mut data: Vec<ProviderData> = vec![];
-   let mut formatted_data: Vec<Vec<Resource>> = vec![];
-   
-   for provider in providers {
-       let provider_data = provider.search(filter).await;
-       data.push(ProviderData {
+        Box::new(spigot::SpigotHandler),
+    ];
+    let size: usize = 10;
+    let mut data_size: usize = 0;
+    let mut data: Vec<ProviderData> = vec![];
+    let mut formatted_data: Vec<Vec<Resource>> = vec![];
+
+    for provider in providers {
+        let provider_data = provider.search(filter).await;
+        data.push(ProviderData {
             provider,
-            list: provider_data
-       });
-   }
+            list: provider_data,
+        });
+    }
 
-   for data in data.iter() {
-       data_size += data.list.len();
-   }
+    for data in data.iter() {
+        data_size += data.list.len();
+    }
 
-   if data_size <= size {
+    if data_size <= size {
         for data in data.iter() {
             let mut list: Vec<Resource> = vec![];
             for item in data.list.iter() {
@@ -69,29 +64,21 @@ pub async fn search(filter: &str) -> Vec<Vec<Resource>> {
             formatted_data.push(list);
         }
         return formatted_data;
-   }
+    }
 
-   let per_data_size: usize = size / data.len();
+    let per_data_size: usize = size / data.len();
 
-   for data in data.iter() {
-
-       let mut loop_num: usize = 0;
-       let mut list: Vec<Resource> = vec![];
-       for item in data.list.iter() {
-           if loop_num > per_data_size {
+    for data in data.iter() {
+        let mut list: Vec<Resource> = vec![];
+        for (loop_num, item) in data.list.iter().enumerate() {
+            if loop_num > per_data_size {
                 break;
-           }
+            }
 
-           list.push(data.provider.format_data(item).await);
+            list.push(data.provider.format_data(item).await);
+        }
+        formatted_data.push(list);
+    }
 
-           loop_num += 1;
-       }
-       formatted_data.push(list);
-
-   }
-
-   return formatted_data;
-
-
-
+    formatted_data
 }
---
+++ src/tui.rs
@@ -0,0 +1,60 @@
+use cursive::{
+    view::{Nameable, Scrollable},
+    views::{Dialog, EditView, ListView, TextView},
+    Cursive,
+};
+
+mod actions;
+mod data;
+
+fn main() {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    // Creates the cursive root - required for every application.
+    let mut siv = cursive::default();
+
+    // Creates a dialog with a single "Quit" button
+    siv.add_layer(
+        Dialog::new()
+            .title("search")
+            .padding_lrtb(1, 1, 1, 0)
+            .content(EditView::new().with_name("search"))
+            .button("Ok", move |s| {
+                println!("lambda block");
+                // s.pop_layer();
+                rt.block_on(perform_search(s));
+            }),
+    );
+
+    // Starts the event loop.
+    siv.run();
+}
+
+async fn perform_search(s: &mut Cursive) {
+    let mut choose = ListView::new();
+    println!("searching...");
+    let search = s
+        .call_on_name("search", |view: &mut EditView| view.get_content())
+        .unwrap();
+    let result = data::search(&search).await;
+    for provider in result {
+        for resource in provider {
+            choose.add_child(
+                &resource.slug,
+                TextView::new(
+                    resource.provider
+                        + "/"
+                        + &resource.slug
+                        + "ver. "
+                        + &resource.version
+                        + "by "
+                        + &resource.author,
+                ),
+            );
+        }
+    }
+    s.pop_layer();
+    s.add_layer(choose.scrollable());
+}
